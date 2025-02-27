ef0b5c43f1179a2ba974deb69d312847b9a48cb8
orca-zhang <orca.zhang@yahoo.com>
2025-02-27 14:00:07 +0800

    fix

--- src/llama.cpp
+++ src/llama.cpp
@@ -614,9 +614,8 @@ static struct ggml_tensor * llm_build_kqv(
         ggml_flash_attn_ext_set_prec(cur, GGML_PREC_F32);
 
         if (n_embd_head_v < n_embd_head_k) {
-            cur = ggml_cont(ctx, ggml_view_3d(ctx, ggml_cont(ctx, cur), n_embd_head_v, n_head, n_tokens,
+            cur = ggml_cont(ctx, ggml_view_2d(ctx, ggml_cont(ctx, cur), n_embd_head_v*n_head, n_tokens,
                                ggml_element_size(cur) * n_embd_head_v_out,
-                               ggml_element_size(cur) * n_embd_head_v_out * n_head,
                                0));
         }
 
